80a5763497065cababfe2111b10f279f584b1004
vitorauvinho <vitor.martins@auvo.com.br>
2026-03-30 15:11:24 -0300

    Update MinhasTrilhas.jsx

--- src/pages/MinhasTrilhas.jsx
+++ src/pages/MinhasTrilhas.jsx
@@ -76,7 +76,7 @@ export default function MinhasTrilhas() {
 
   return (
     <>
-      <AnalistaLayout analystName={analyst.name}>
+      <AnalistaLayout analystName={analyst.name} analystTeam={analyst.team}>
         <div style={{ padding: '24px', maxWidth: 860, margin: '0 auto' }}>
 
           <div style={{ marginBottom: 20 }}>
