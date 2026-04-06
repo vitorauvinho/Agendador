27f89f95ce59474d62fafb500c76e08beaf1893a
vitorauvinho <vitor.martins@auvo.com.br>
2026-04-06 14:45:09 -0300

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -74,7 +74,7 @@ export function AuthProvider({ children }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: window.location.origin,
+        redirectTo: 'https://agendador-one.vercel.app',
       }
     })
     if (error) throw error
